b65ef1e1490d444f0d64018cf51cde340c87e1b9
Johnathan Chen <johnathanchenn@outlook.com>
2018-02-02 15:15:06 -0800

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Johnathan Chen on 2/2/18.
+//  Copyright © 2018 JCSwifty. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+  
+
+}
+
